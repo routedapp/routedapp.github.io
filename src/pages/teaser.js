d73eaf62f8209c20af712490271f446e3fb88c6e
John Dunning <fw@johndunning.com>
2022-06-06 13:28:35 -0700

    Use vmin font size in Teaser container
    Then use em instead of rem to size the child elements.

--- src/pages/teaser.js
+++ src/pages/teaser.js
@@ -4,38 +4,39 @@ import { Helmet } from "react-helmet";
 import BodyText from "@/components/BodyText";
 import Logo from "@/images/logomark.png";
 
+	// since we can't seem to easily apply a new font-size to html or body from
+	// here, set the font-size on the container using vmin, so it's based on the
+	// smaller side of the viewport, and then use em instead of rem to scale the
+	// font in the rest of the elements
 const styles = {
 	"&": {
 		color: "primary",
-		textAlign: "center"
-	},
-	html: {
+		textAlign: "center",
 		fontSize: "3vmin"
 	},
 	h1: {
-		fontSize: 7,
+		fontSize: "3em",		// theme size 7
 		fontWeight: 500
 	},
 	h3: {
-		fontSize: 5,
+		fontSize: "1.75em",	// theme size 5
 		fontWeight: 300
-	},
-	"h3 a": {
-		fontWeight: 500
 	},
 	a: {
 		color: "primary",
+		fontSize: "1em",
+		fontWeight: 500,
 		textDecoration: "none"
 	},
 	"a:hover": {
 		textDecoration: "underline"
 	},
 	header: {
-		height: "8rem",
-		borderTop: "1rem solid black",
+		height: "8em",
+		borderTop: "1em solid black",
 		backgroundImage: `url(${Logo})`,
-		backgroundPosition: "center 2rem",
-		backgroundSize: "10rem",
+		backgroundPosition: "center 2em",
+		backgroundSize: "10em",
 		backgroundRepeat: "no-repeat"
 	},
 	main: {
@@ -49,6 +50,8 @@ const styles = {
 function Styles(
 	props)
 {
+		// we can't just add an sx prop to the BaseStyles, because the styles in sx
+		// replace what's in BaseStyles instead of being merged with it
 	return (
 		<BaseStyles>
 			<Box

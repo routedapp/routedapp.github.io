320332d9ac39320bb0dba565827a078b84efb069
John Dunning <fw@johndunning.com>
2022-06-23 14:37:04 -0700

    Get the Contentful host from an env secret
    Default to the production cdn.contentful.com.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -26,6 +26,7 @@ export default {
 			options: {
 				spaceId: process.env.CONTENTFUL_SPACE_ID,
 				accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+				host: process.env.CONTENTFUL_HOST || "cdn.contentful.com"
 			},
 		},
 		{

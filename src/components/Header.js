3930a6902985cc178f7c3d713b23c2ffaaed3a54
John Dunning <fw@johndunning.com>
2022-10-14 16:03:29 -0700

    Show the nav menu over a grey background
    Don't make it full-screen.
    Refactor code into a Menu component that contains the toggle.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,35 +5,18 @@ import Link from "./Link";
 import Logo from "./Logo";
 
 const expandedClass = "navmenu-expanded";
-const menuStyles = {
-	gap: "md",
-	alignItems: "center",
-	display: ["none", "flex"],
 
-	[`.${expandedClass} &`]: {
-		bg: "white",
-		width: "100%",
-		height: "100%",
-		p: "xl",
-		pt: "xxl",
-		top: 0,
-		position: "fixed",
-		flexDirection: "column",
-		alignItems: "self-start",
-		gap: "xl",
-		display: "flex",
-			// make sure the menu can scroll vertically if the window is too short
-		overflowY: "auto",
-		zIndex: 100,
-
-		"& > a": {
-			fontSize: "banner",
-		}
-	},
-};
+const homeLinks = [
+	<Link to="/">Home</Link>,
+	<Link to="/our-story/">Our Story</Link>,
+	<Link to="/faq/">FAQ</Link>,
+	<Link to="/support/">Support</Link>,
+	<Link to="/user-guides/" partiallyActive={true}>User Guides</Link>,
+].map((el) => React.cloneElement(el, { key: el.props.to }));
+const links = homeLinks.slice(1);
 
 function MenuToggle({
-	menuExpanded,
+	open,
 	onClick })
 {
 	return (
@@ -42,7 +25,6 @@ function MenuToggle({
 			sx={{
 					// line up the icon with the right edge of the header image
 				mr: "-6px",
-				display: ["inline-block", "none"],
 				zIndex: 150,
 
 				":hover": {
@@ -54,34 +36,90 @@ function MenuToggle({
 				}
 			}}
 		>
-			{menuExpanded
+			{open
 				? <Close title="Close the navigation menu" />
 				: <MenuButton title="Open the navigation menu" />}
 		</Box>
 	);
 }
 
+function Menu({
+	open,
+	onToggle })
+{
+	return (
+		<>
+			<MenuToggle
+				open={open}
+				onClick={onToggle}
+			/>
+			<Box
+					// since this box is only displayed when the menu is open, a click
+					// on the modal overlay will always toggle it closed
+				onClick={onToggle}
+				sx={{
+					background: "rgba(0, 0, 0, .5)",
+					width: "100%",
+					height: "100%",
+					top: 0,
+					left: 0,
+					position: "absolute",
+					zIndex: 100,
+					display: open ? "block" : "none",
+				}}
+			>
+				<Flex
+						// ignore stray clicks on the menu background
+					onClick={(e) => e.stopPropagation()}
+					sx={{
+						bg: "white",
+						width: "24rem",
+						height: "100%",
+						p: "xl",
+						pt: "xxl",
+						top: 0,
+						right: 0,
+						position: "fixed",
+						flexDirection: "column",
+						alignItems: "self-start",
+						gap: "xl",
+						display: "flex",
+							// make sure the menu can scroll vertically if the window is too short
+						overflowY: "auto",
+
+						"& > a": {
+							fontSize: "banner",
+						}
+					}}
+				>
+					{homeLinks}
+				</Flex>
+			</Box>
+		</>
+	);
+}
+
 export default function Header({
 	location: { pathname } })
 {
-	const [menuExpanded, setMenuExpanded] = useState(false);
+	const [menuOpen, setMenuOpen] = useState(false);
 	const pathnameRef = useRef();
 	const navRef = useRef();
 	const breakpoint = useBreakpointIndex();
 
 	const handleMenuToggle = (event) => {
-		setMenuExpanded(!menuExpanded);
+		setMenuOpen(!menuOpen);
 		event.preventDefault();
 	};
 
 	const handleMenuKeyDown = (event) => {
-		if (menuExpanded && event.key === "Escape") {
-			setMenuExpanded(false);
+		if (menuOpen && event.key === "Escape") {
+			setMenuOpen(false);
 		}
 	};
 
 	useEffect(() => {
-		if (menuExpanded) {
+		if (menuOpen) {
 				// add the navmenu-expanded class to the html element, to prevent the
 				// page from scrolling.  focus the nav element so it can listen for
 				// key events.
@@ -90,20 +128,20 @@ export default function Header({
 		} else {
 			document.documentElement.classList.remove(expandedClass);
 		}
-	}, [menuExpanded]);
+	}, [menuOpen]);
 
 
 	useEffect(() => {
-		if (pathname !== pathnameRef.current || (menuExpanded && breakpoint > 0)) {
+		if (pathname !== pathnameRef.current || (menuOpen && breakpoint > 0)) {
 				// the location has changed, either because the user clicked a link in
 				// the menu or they navigated through the browser history.  or the menu
 				// is visible and the user made the window wide enough to show all the
 				// links.  whatever happened, we want to track the current location and
 				// close the menu.
 			pathnameRef.current = pathname;
-			setMenuExpanded(false);
+			setMenuOpen(false);
 		}
-	}, [pathname, menuExpanded, breakpoint]);
+	}, [pathname, menuOpen, breakpoint]);
 
 	return (
 		<Container as="header"
@@ -124,20 +162,23 @@ export default function Header({
 				>
 					<Logo sx={{ height: "2.25rem" }} />
 				</Link>
-				<MenuToggle
-					menuExpanded={menuExpanded}
-					onClick={handleMenuToggle}
-				/>
-				<Flex sx={menuStyles}>
-					{menuExpanded &&
-							// add a link to the home page only when the menu is expanded
-						<Link to="/">Home</Link>
-					}
-					<Link to="/our-story/">Our Story</Link>
-					<Link to="/faq/">FAQ</Link>
-					<Link to="/support/">Support</Link>
-					<Link to="/user-guides/" partiallyActive={true}>User Guides</Link>
-				</Flex>
+				{breakpoint === 0
+					? (
+						<Menu
+							open={menuOpen}
+							onToggle={handleMenuToggle}
+						/>
+					) : (
+						<Flex
+							sx={{
+								gap: "md",
+								alignItems: "center",
+							}}
+						>
+							{links}
+						</Flex>
+					)
+				}
 			</Flex>
 		</Container>
 	);
